8b47a590d1eab597358fcb3023ce51ae936c6346
ana gil <anagamo@ext.inditex.com>
2025-07-25 17:43:21 +0200

    feat: review badge

--- src/ui/components/badge/Badge.tsx
+++ src/ui/components/badge/Badge.tsx
@@ -3,12 +3,9 @@ import styles from './Badget.module.css';
 import { BadgeProps } from './Badge.types';
 import { useTexts } from '@/ui/hooks/useTexts';
 
-const Badge: React.FC<BadgeProps> = ({
-  type = 'favorite',
-  className = '',
-}) => {
+const Badge: React.FC<BadgeProps> = ({ type = 'favorite', className = '' }) => {
   const { getBadgeText } = useTexts();
-  
+
   return (
     <span className={`${styles.badge} ${styles[type]} ${className}`}>
       {getBadgeText(type)}
