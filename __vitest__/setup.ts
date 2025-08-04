6670f1d3556a462782e23c4837ea87c699f6ef9f
ana gil <anagamo@ext.inditex.com>
2025-08-04 20:47:30 +0200

    feat: set env for testing

--- __vitest__/setup.ts
+++ __vitest__/setup.ts
@@ -1,6 +1,10 @@
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
+// Configure environment variables for testing
+process.env.NEXT_PUBLIC_MOVIES_API_URL =
+  'https://67582a7f60576a194d12bc50.mockapi.io/movies';
+
 // Mock para CSS modules
 vi.mock('*.module.css', () => ({
   default: new Proxy(
